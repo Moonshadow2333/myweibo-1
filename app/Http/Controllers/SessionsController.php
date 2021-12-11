c467783f13e38012940fb4f4b067b4d21e877d56
moonshadow <1815265375@qq.com>
2021-12-11 09:11:25 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 use Auth;
 class SessionsController extends Controller
 {
+    public function __construct(){
+         $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     public function create(){
         return view('sessions.create');
     }
@@ -16,7 +21,8 @@ class SessionsController extends Controller
         ]);
         if(Auth::attempt($credentials,$request->has('remember'))){
              session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
+            $fallback = route('users.show',Auth::user());
+           return redirect()->intended($fallback);
         }else{
             // 登入失败的相关操作。
             session()->flash('danger','很抱歉，您的邮箱与密码不匹配！');
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,13 @@ use App\Models\User;
 use Auth;
 class UsersController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth',['except'=>['show','create','store']
+        ]);
+         $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     public function show(User $user){
         return view('users.show',compact('user'));
     }
@@ -29,9 +36,11 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
     public function update(User $user,Request $request){
+        $this->authorize('update',$user);
         $this->validate($request,[
             'name'=>'required|max:50',
             'password'=>'nullable|confirmed|min:6'

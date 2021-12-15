20743720ec751473d8326ac0488b11846b00efe3
moonshadow <1815265375@qq.com>
2021-12-15 12:59:54 +0000

    取消关注

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,6 +78,6 @@ class User extends Authenticatable
         $this->followings()->detach($user_ids);
     }
     public function isFollowing($user_id){
-        return $this->followings-contains($user_id);
+        return $this->followings->contains($user_id);
     }
 }

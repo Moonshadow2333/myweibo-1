a3b29bf551b6a2f65d49d6c66133f58b536a4f11
moonshadow <1815265375@qq.com>
2021-12-15 00:59:45 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
+@if(Auth::check())
+<div class="row">
+    <div class="col-md-8">
+        <section class="status_form">
+            @include('shared._status_form')
+        </section>
+    </div>
+    <aside class="col-md-4">
+        <section class="user_info">
+            @include('shared._user_info',['user'=>Auth::user()])
+        </section>
+    </aside>
+</div>
+@else
 <div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">你现在看到的是<a href="https://learnku.com/courses/laravel-essential-training">Laravel入门教程</a>的示例项目主页</p>
@@ -9,4 +23,5 @@
         <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
     </p>
 </div>
+@endif
 @stop
